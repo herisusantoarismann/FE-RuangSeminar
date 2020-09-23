c6b2aca59fb936d33030a07aadec88b58bb8fbe1
Heri Susanto Arisman <eriparigi12@gmail.com>
2020-09-23 14:52:49 +0700

    Add Spectre

--- src/Page/Seminar/Tambah/Peserta/index.js
+++ src/Page/Seminar/Tambah/Peserta/index.js
@@ -1,32 +1,35 @@
 import React from "react";
 import { Button, Gap, Input } from "../../../../Component";
+import "./style.scss";
 
 const TambahPeserta = () => {
   return (
-    <div className="input-seminar">
-      <form>
-        <h2>Tambah Peserta</h2>
-        <Input
-          label="Nama"
-          placeholder="Masukkan Nama "
-          type="text"
-          name="nama"
-        />
-        <Input
-          label="Email"
-          placeholder="Masukkan Email"
-          type="email"
-          name="email"
-        />
-        <Input
-          label="Pemateri"
-          placeholder="Masukkan Nomor"
-          type="text"
-          name="nomor"
-        />
-        <Gap height={20} />
-        <Button title="Submit" type="submit" />
-      </form>
+    <div className="input-peserta-wrapper">
+      <div className="input-peserta">
+        <form>
+          <h2>Tambah Peserta</h2>
+          <Input
+            label="Nama"
+            placeholder="Masukkan Nama "
+            type="text"
+            name="nama"
+          />
+          <Input
+            label="Email"
+            placeholder="Masukkan Email"
+            type="email"
+            name="email"
+          />
+          <Input
+            label="Pemateri"
+            placeholder="Masukkan Nomor"
+            type="text"
+            name="nomor"
+          />
+          <Gap height={20} />
+          <Button title="Submit" type="submit" />
+        </form>
+      </div>
     </div>
   );
 };
