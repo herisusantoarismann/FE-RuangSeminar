ead920014c4065ab72663e2c9afaaccf064211a2
Heri Susanto Arisman <eriparigi12@gmail.com>
2020-09-24 07:52:25 +0700

    Add package moment.js

--- src/Page/Seminar/Tambah/Seminar/index.js
+++ src/Page/Seminar/Tambah/Seminar/index.js
@@ -3,39 +3,56 @@ import { Input, Gap, Button } from "../../../../Component";
 import "./style.scss";
 
 class TambahSeminar extends Component {
+  constructor() {
+    super();
+    this.HandleSubmit = this.HandleSubmit.bind(this);
+  }
+
+  HandleSubmit(e) {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    let date = data.get("tanggal");
+    let formatted_date =
+      date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+    console.log(formatted_date);
+    console.log(data.get("tanggal"));
+  }
+
   render() {
     return (
       <div className="input-seminar-wrapper">
         <div className="input-seminar">
           <h2>Tambah Seminar</h2>
-          <Input
-            label="Nama"
-            placeholder="Masukkan Nama"
-            type="text"
-            name="nama_seminar"
-          />
-          <Input
-            label="Pemateri"
-            placeholder="Masukkan Pemateri"
-            type="text"
-            name="pemateri"
-          />
-          <Input
-            label="Nama"
-            placeholder="Masukkan Nama"
-            type="date"
-            name="tanggal"
-          />
-          <Input
-            label="Durasi"
-            placeholder="Masukkan Durasi (menit)"
-            type="text"
-            name="durasi_menit"
-          />
-          <Gap height={20} />
-          <div className="btn-submit">
-            <Button title="Submit" />
-          </div>
+          <form onSubmit={this.HandleSubmit}>
+            <Input
+              label="Nama"
+              placeholder="Masukkan Nama"
+              type="text"
+              name="nama_seminar"
+            />
+            <Input
+              label="Pemateri"
+              placeholder="Masukkan Pemateri"
+              type="text"
+              name="pemateri"
+            />
+            <Input
+              label="Nama"
+              placeholder="Masukkan Nama"
+              type="date"
+              name="tanggal"
+            />
+            <Input
+              label="Durasi"
+              placeholder="Masukkan Durasi (menit)"
+              type="text"
+              name="durasi_menit"
+            />
+            <Gap height={20} />
+            <div className="btn-submit">
+              <Button title="Submit" />
+            </div>
+          </form>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <TambahPeserta />
+    <TambahSeminar />
   </React.StrictMode>,
   document.getElementById("root")
 );
