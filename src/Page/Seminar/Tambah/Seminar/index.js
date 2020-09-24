1bb502e24a5acea72cb87a375e221d7b2697d7a9
Heri Susanto Arisman <eriparigi12@gmail.com>
2020-09-24 14:47:35 +0700

    Ubah Tambah seminar

--- src/Page/Seminar/Tambah/Seminar/index.js
+++ src/Page/Seminar/Tambah/Seminar/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Input, Gap, Button } from "../../../../Component";
+// import { Input, Gap, Button } from "../../../../Component";
 import moment from "moment";
 import "moment/locale/id";
 import "./style.scss";
@@ -101,6 +101,59 @@ class TambahSeminar extends Component {
 
       <div className="input-seminar">
         <h2>Tambah Seminar</h2>
+        <form class="form-horizontal">
+          <div class="form-group">
+            <div class="col-3 col-sm-12">
+              <label class="form-label">Nama Seminar</label>
+            </div>
+            <div class="col-9 col-sm-12">
+              <input
+                class="form-input"
+                type="text"
+                placeholder="Masukkan Nama Seminar"
+              />
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-3 col-sm-12">
+              <label class="form-label">Pemateri</label>
+            </div>
+            <div class="col-9 col-sm-12">
+              <input
+                class="form-input"
+                type="text"
+                placeholder="Masukkan Pemateri"
+              />
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-3 col-sm-12">
+              <label class="form-label">Tanggal</label>
+            </div>
+            <div class="col-9 col-sm-12">
+              <input
+                class="form-input"
+                type="text"
+                placeholder="Masukkan Tanggal"
+              />
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-3 col-sm-12">
+              <label class="form-label">Durasi (menit)</label>
+            </div>
+            <div class="col-9 col-sm-12">
+              <input
+                class="form-input"
+                type="text"
+                placeholder="Masukkan Durasi"
+              />
+            </div>
+          </div>
+          <div className="btn-submit">
+            <button className="btn btn-primary">Tambah</button>
+          </div>
+        </form>
       </div>
     );
   }
